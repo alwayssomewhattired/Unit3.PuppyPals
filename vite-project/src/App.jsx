9ea96e16798c3d59b7320e1a8b3ac060df3f97af
Zachary Malinka <132944209+alwayssomewhattired@users.noreply.github.com>
2024-11-26 13:45:44 -0500

    Add files via upload

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import "./App.css";
+import { puppyList } from "./data";
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick() {
+    //some logic here
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+  console.log(featuredPup);
+  return (
+    <div className="App">
+      {featPupId && (
+        <div>
+          {" "}
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>{" "}
+        </div>
+      )}
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
